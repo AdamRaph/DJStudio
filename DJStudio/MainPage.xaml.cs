040decb7b31d36e2207a9c10f4bd4c09721f04c2
AdehhRR <adam.raphael@hotmail.com>
2017-03-08 11:53:06 +1100

    first commit

---
+++ DJStudio/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Windows.Storage.Streams;
+using Windows.UI.Input;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DJStudio
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        RadialController myController;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            // Create a reference to the RadialController.
+            myController = RadialController.CreateForCurrentView();
+
+            // Create an icon for the custom tool.
+            var icon =
+              RandomAccessStreamReference.CreateFromUri(
+                new Uri("ms-appx:///Assets/StoreLogo.png"));
+
+            // Create a menu item for the custom tool.
+            var myItem =
+              RadialControllerMenuItem.CreateFromIcon("Sample", icon);
+            var myItem2 =
+              RadialControllerMenuItem.CreateFromIcon("Sample2", icon);
+
+            // Add the custom tool to the RadialController menu.
+            myController.Menu.Items.Add(myItem);
+            myController.Menu.Items.Add(myItem2);
+
+            // Declare input handlers for the RadialController.
+            myController.ButtonClicked += MyController_ButtonClicked;
+            myController.RotationChanged += MyController_RotationChanged;
+        }
+
+        // Handler for rotation input from the RadialController.
+        private void MyController_RotationChanged(RadialController sender,
+          RadialControllerRotationChangedEventArgs args)
+        {
+            if (RotationSlider.Value + args.RotationDeltaInDegrees > 100)
+            {
+                RotationSlider.Value = 100;
+                return;
+            }
+            else if (RotationSlider.Value + args.RotationDeltaInDegrees < 0)
+            {
+                RotationSlider.Value = 0;
+                return;
+            }
+            RotationSlider.Value += args.RotationDeltaInDegrees;
+        }
+
+        // Handler for click input from the RadialController.
+        private void MyController_ButtonClicked(RadialController sender,
+          RadialControllerButtonClickedEventArgs args)
+        {
+            ButtonToggle.IsOn = !ButtonToggle.IsOn;
+        }
+
+    }
+}
